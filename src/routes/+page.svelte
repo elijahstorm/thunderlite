31c3330f325b4ff05a7a3d3c67e1fea08b601a28
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-09-22 13:49:33 +0700

    configure project

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
+<script lang="ts">
+	import { PUBLIC_GAME_NAME } from '$env/static/public'
+</script>
+
+<svelte:head>
+	<title>{PUBLIC_GAME_NAME}</title>
+</svelte:head>
+
+<h1 class="text-red-500">Welcome to {PUBLIC_GAME_NAME}</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
