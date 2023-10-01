52e85176d12fab2e022c757c6d4c4adaab4e5c00
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-10-01 14:57:35 +0700

    Update spriteConnector.ts

--- src/lib/Sprites/spriteConnector.ts
+++ src/lib/Sprites/spriteConnector.ts
@@ -1,4 +1,5 @@
-import { terrainData, terrainRenderer } from '$lib/GameData/Terrain'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { terrainData } from '$lib/GameData/Terrain'
 
 type ConnectionDecision = (map: MapObject, location: number) => number
 
@@ -7,52 +8,6 @@ export const connectionDecision = (object: GroundObject) =>
 
 const singular: ConnectionDecision = (map, location) => 0
 
-const rollDecision = {
-	true: {
-		true: {
-			true: {
-				true: 5,
-				false: 6,
-			},
-			false: {
-				true: 4,
-				false: 3,
-			},
-		},
-		false: {
-			true: {
-				true: 8,
-				false: 2,
-			},
-			false: {
-				true: 11,
-				false: 1,
-			},
-		},
-	},
-	false: {
-		true: {
-			true: {
-				true: 7,
-				false: 9,
-			},
-			false: {
-				true: 12,
-				false: 13,
-			},
-		},
-		false: {
-			true: {
-				true: 10,
-				false: 14,
-			},
-			false: {
-				true: 15,
-				false: 0,
-			},
-		},
-	},
-}
 const rollInto: ConnectionDecision = (map, location) =>
 	rollDecision[left(map, location) ? 'true' : 'false'][up(map, location) ? 'true' : 'false'][
 		right(map, location) ? 'true' : 'false'
@@ -60,70 +15,41 @@ const rollInto: ConnectionDecision = (map, location) =>
 
 const random: ConnectionDecision = (map, location) => Math.floor(Math.random() * 5)
 
+// const cornerDecision = {
+// 	true: {
+// 		true: {
+// 			true: 0,
+// 			false: 5,
+// 		},
+// 		false: {
+// 			true: 0,
+// 			false: 0,
+// 		},
+// 	},
+// 	false: {
+// 		true: ,
+// 		false: ,
+// 	},
+// }
 const border: ConnectionDecision = (map, location) => {
+	if (
+		left(map, location)(details) &&
+		up(map, location)(details) &&
+		!up(map, location - 1)(details)
+	) {
+		return 5
+	}
+	if (
+		left(map, location)(details) &&
+		down(map, location)(details) &&
+		!down(map, location - 1)(details)
+	) {
+		return 6
+	}
+
 	return Math.floor(Math.random() * 12)
-
-	// sea border and animation
-	__sprite = null
-	let sea_index = Terrain_Data.Get('Sea')
-	// let type = Terrain_Data.TERRE[index].Type;
-	type = Terrain_Data.TERRE[sea_index].Type
-	let connector = 8
-	let __ANIMATION = Animations.Retrieve(Terrain_Data.TERRE[index].Name + ' Ani')
-
-	/// check if corner add here
-
-	if (x != 0)
-		if (y != 0)
-			if (Terrain_Data.TERRE[_map[x - 1][y - 1]].Type != connector)
-				if (Terrain_Data.TERRE[_map[x - 1][y - 1]].Type != type)
-					if (Terrain_Data.TERRE[_map[x][y - 1]].Type == type)
-						if (Terrain_Data.TERRE[_map[x - 1][y]].Type == type) {
-							// top left
-							__img = Terrain_Data.TERRE[index].Borders[4]
-							__img.Draw(imageHolderCanvas, 0, 0, TILESIZE, TILESIZE)
-						}
-	if (x != 0)
-		if (y != _map[x].length - 1)
-			if (Terrain_Data.TERRE[_map[x - 1][y + 1]].Type != connector)
-				if (Terrain_Data.TERRE[_map[x - 1][y + 1]].Type != type)
-					if (Terrain_Data.TERRE[_map[x][y + 1]].Type == type)
-						if (Terrain_Data.TERRE[_map[x - 1][y]].Type == type) {
-							// bottom left
-							__img = Terrain_Data.TERRE[index].Borders[5]
-							__img.Draw(imageHolderCanvas, 0, 0, TILESIZE, TILESIZE)
-						}
-	if (x != _map.length - 1)
-		if (y != 0)
-			if (Terrain_Data.TERRE[_map[x + 1][y - 1]].Type != connector)
-				if (Terrain_Data.TERRE[_map[x + 1][y - 1]].Type != type)
-					if (Terrain_Data.TERRE[_map[x][y - 1]].Type == type)
-						if (Terrain_Data.TERRE[_map[x + 1][y]].Type == type) {
-							// top right
-							__img = Terrain_Data.TERRE[index].Borders[4]
-							imageHolderCanvas.save()
-							imageHolderCanvas.scale(-1, 1)
-							imageHolderCanvas.translate(-TILESIZE, 0)
-							__img.Draw(imageHolderCanvas, 0, 0, TILESIZE, TILESIZE)
-							imageHolderCanvas.restore()
-						}
-	if (x != _map.length - 1)
-		if (y != _map[x].length - 1)
-			if (Terrain_Data.TERRE[_map[x + 1][y + 1]].Type != connector)
-				if (Terrain_Data.TERRE[_map[x + 1][y + 1]].Type != type)
-					if (Terrain_Data.TERRE[_map[x][y + 1]].Type == type)
-						if (Terrain_Data.TERRE[_map[x + 1][y]].Type == type) {
-							// bottom right
-							__img = Terrain_Data.TERRE[index].Borders[5]
-							imageHolderCanvas.save()
-							imageHolderCanvas.scale(-1, 1)
-							imageHolderCanvas.translate(-TILESIZE, 0)
-							__img.Draw(imageHolderCanvas, 0, 0, TILESIZE, TILESIZE)
-							imageHolderCanvas.restore()
-						}
-
-	let cornerImg = imageHolderCanvas.getImageData(0, 0, TILESIZE, TILESIZE)
-
+}
+const old = () => {
 	if (x != 0)
 		if (Terrain_Data.TERRE[_map[x - 1][y]].Type != connector)
 			if (Terrain_Data.TERRE[_map[x - 1][y]].Type != type) {
@@ -334,16 +260,68 @@ const flowDecision: [
 	ConnectionDecision,
 ] = [singular, rollInto, random, border, bridge]
 
-const up = (map: MapObject, location: number) =>
-	location - map.rows >= 0 &&
-	map.layers.ground[location - map.rows].type === map.layers.ground[location].type
-const down = (map: MapObject, location: number) =>
-	location + map.rows < map.layers.ground.length &&
-	map.layers.ground[location + map.rows].type === map.layers.ground[location].type
+const rollDecision = {
+	true: {
+		true: {
+			true: {
+				true: 5,
+				false: 6,
+			},
+			false: {
+				true: 4,
+				false: 3,
+			},
+		},
+		false: {
+			true: {
+				true: 8,
+				false: 2,
+			},
+			false: {
+				true: 11,
+				false: 1,
+			},
+		},
+	},
+	false: {
+		true: {
+			true: {
+				true: 7,
+				false: 9,
+			},
+			false: {
+				true: 12,
+				false: 13,
+			},
+		},
+		false: {
+			true: {
+				true: 10,
+				false: 14,
+			},
+			false: {
+				true: 15,
+				false: 0,
+			},
+		},
+	},
+}
 
-const left = (map: MapObject, location: number) =>
+const type = (map: GroundObject[], location: number) => map[location].type
+const details = (map: GroundObject[], location: number) => terrainData[map[location].type].details
+
+const up =
+	(map: MapObject, location: number) =>
+	(reader: typeof type | typeof details = type) =>
+		location - map.rows >= 0 &&
+		reader(map.layers.ground, location - map.rows) === reader(map.layers.ground, location)
+const down = (map: MapObject, location: number) => (reader: typeof type | typeof details) =>
+	location + map.rows < map.layers.ground.length &&
+	reader(map.layers.ground, location + map.rows) === reader(map.layers.ground, location)
+
+const left = (map: MapObject, location: number) => (reader: typeof type | typeof details) =>
 	location % map.rows !== 0 &&
-	map.layers.ground[location - 1].type === map.layers.ground[location].type
-const right = (map: MapObject, location: number) =>
+	reader(map.layers.ground, location - 1) === reader(map.layers.ground, location)
+const right = (map: MapObject, location: number) => (reader: typeof type | typeof details) =>
 	(location + 1) % map.rows !== 0 &&
-	map.layers.ground[location + 1].type === map.layers.ground[location].type
+	reader(map.layers.ground, location + 1) === reader(map.layers.ground, location)
