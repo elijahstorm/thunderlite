45cd516ceb2961e1492d7ebddfc08e59c11ca060
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-10-11 15:39:17 +0700

    init smoke tests

--- tests/test.ts
+++ tests/test.ts
@@ -1,6 +1,20 @@
 import { expect, test } from '@playwright/test'
 
-test('index page has expected h1', async ({ page }) => {
+test('smoke test', async ({ page }) => {
 	await page.goto('/')
-	await expect(page.getByRole('heading', { name: 'Welcome to SvelteKit' })).toBeVisible()
+	await expect(page.getByText('ERROR')).not.toBeVisible()
+})
+
+test('editor page has working editor', async ({ page }) => {
+	await page.goto('/editor')
+	await expect(page.getByRole('button', { name: 'save' })).toBeVisible()
+	await expect(page.getByRole('button', { name: 'open' })).toBeVisible()
+	await expect(page.getByRole('button', { name: 'share' })).toBeVisible()
+	await expect(page.getByRole('button', { name: 'play' })).toBeVisible()
+	await expect(page.getByRole('button', { name: 'options' })).toBeVisible()
+})
+
+test('play page has working game', async ({ page }) => {
+	await page.goto('/play')
+	await expect(page.locator('section[role="grid"] canvas')).toBeVisible()
 })
