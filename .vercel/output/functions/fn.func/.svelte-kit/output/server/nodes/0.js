d50380b33b7a665d20e1fca6d39aaaa2d0fd7869
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-10-11 15:38:07 +0700

    rm vercel build

--- .vercel/output/functions/fn.func/.svelte-kit/output/server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/pages/_layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.fc688834.js","_app/immutable/chunks/scheduler.4123ea5a.js","_app/immutable/chunks/index.ccd460da.js","_app/immutable/chunks/each.0583d308.js","_app/immutable/chunks/index.0e1d7533.js"];
-export const stylesheets = ["_app/immutable/assets/0.7409e74f.css"];
-export const fonts = [];
