0f772f8fcef0271d8611d82af39cef4b306d2764
elijahstorm <95153181+elijahstorm@users.noreply.github.com>
2023-11-25 14:10:33 +0900

    remove readonly token

--- src/routes/(app)/rooms/+page.server.ts
+++ src/routes/(app)/rooms/+page.server.ts
@@ -1,11 +1,11 @@
 import { createClient } from '@vercel/kv'
 import type { PageServerLoad } from './$types'
-import { KV_REST_API_READ_ONLY_TOKEN, KV_REST_API_URL } from '$env/static/private'
+import { KV_REST_API_TOKEN, KV_REST_API_URL } from '$env/static/private'
 
 export const load: PageServerLoad = async ({ locals }) => {
 	const kv = createClient({
 		url: KV_REST_API_URL,
-		token: KV_REST_API_READ_ONLY_TOKEN,
+		token: KV_REST_API_TOKEN,
 	})
 
 	const gameData = (await kv.hgetall(`user-game:${locals?.session ?? ''}`)) as unknown as {
